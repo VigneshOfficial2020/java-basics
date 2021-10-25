4c96c59276da4a6ba7836d82c0562abc6d766754
VigneshbabuA <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-10-26 00:37:43 +0530

    final keyword demo

---
+++ src/basics/Testing2.java
@@ -0,0 +1,30 @@
+package basics;
+
+public class Testing2 {
+
+	final int a;
+	final static int b;
+	final int c=10;
+
+	{
+		a = 25;
+	}
+
+	static {
+		b = 25;
+	}
+
+	private void assign() {
+//		a = 25;
+//		b = 25;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		a = 25;
+//		b = 25;
+//		c = 15;
+
+	}
+
+}
