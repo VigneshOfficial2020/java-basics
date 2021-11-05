3e94bc655b677e0ae57e8d244e98fea25d419e8d
VigneshbabuA <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-11-05 11:59:47 +0530

    string testing demo

--- src/basics/StringTesting.java
+++ src/basics/StringTesting.java
@@ -13,18 +13,34 @@ public class StringTesting {
 
 		String s4 = new String("java");
 		
+		System.out.println("String s1 = \"java\";\r\n"
+				+ "String s2 = \"java\";\r\n"
+				+ "String s3 = \"java\";\r\n"
+				+ "String s4 = new String(\"java\");");
+		
+		System.out.println("------------------------------");
+		
 		// s1==s2
+		System.out.println("s1 == s2");
 		System.out.println(s1 == s2);
+		System.out.println("------------------------------");
 		
 		// s1==s3
+		System.out.println("s1==s3");
 		System.out.println(s1==s3);
+		System.out.println("------------------------------");
 		
 		// s1==s4
+		System.out.println("s1==s4");
 		System.out.println(s1==s4);
+		System.out.println("------------------------------");
 		
 		s1 = s1 + "J2EE";
+		System.out.println("s1 = s1 + \"J2EE\";");
 		
+		System.out.println("s1==s2");
 		System.out.println(s1==s2);
+		System.out.println("------------------------------");
 		
 	}
 
